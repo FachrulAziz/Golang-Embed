1ba9137deab1c7d3b844bc168fd23187959dcccc
FachrulAziz <fachrulaziz95@gmail.com>
2023-04-28 15:55:21 +0700

    Belajar Golang Embed

---
+++ embed_test.go
@@ -0,0 +1,43 @@
+package belajargolangembed
+
+import (
+	"embed"
+	_ "embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+func TestString(t *testing.T) {
+	fmt.Println(version)
+}
+
+//go:embed logo.jpg
+var logoo []byte
+
+func TestByte(t *testing.T) {
+	err := ioutil.WriteFile("logo_ne.png", logoo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+var files embed.FS
+
+func TestMultipleFiles(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
+}
